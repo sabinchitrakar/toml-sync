f7e6ff4192fd77ae6ab6df8ad891e01a5414b403
sabin chitrakar <immortal.infidel@gmail.com>
2022-03-26 10:36:42 +0000

    feat: add diff

--- toml-sync/src/main.rs
+++ toml-sync/src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use dialoguer::{Input, Select, console::Term, theme::ColorfulTheme};
+use tompl_sync_lib::{Source, SourceType, TomlSync, SyncConfig};
+
+#[tokio::main]
+async fn main() {
+    let sources : String = Input::new()
+    .with_prompt("Add Sources Comma Separated")
+    .interact_text().unwrap();
+
+    let target=Input::<String>::new()
+    .with_prompt("Directory To Search")
+    .interact_text().unwrap();
+
+    let sources_parsed= sources.split(",").map(|t|{
+        let source_type = if t.starts_with("http") { SourceType::Remote } else { SourceType::Local};
+        let source = Source{
+            path:t.to_string(),
+            source_type
+        };
+        source
+    }).collect::<Vec<Source>>();
+
+    let mut sync = TomlSync::new(SyncConfig{
+        destination:target,
+        sources:sources_parsed
+    });
+    sync.scan().await;
+
+    show_menu(sync);
+
+}
+
+
+pub fn show_menu(sync:TomlSync){
+
+    let items = vec!["Show Diff", "Sync Versions"];
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .items(&items)
+        .default(0)
+        .interact_on_opt(&Term::stderr()).unwrap();
+
+    match selection {
+        Some(index) => {
+            match index {
+                0 =>{
+                  sync.show_diff();
+                },
+                _ =>{}
+            }
+        },
+        None => println!("User did not select anything")
+    }
+
 }
--- tompl-sync-lib/src/lib.rs
+++ tompl-sync-lib/src/lib.rs
@@ -1,17 +1,15 @@
-#[macro_use] extern crate prettytable;
-use std::{path::{PathBuf}, collections::HashMap, ops::Add, fmt::Display};
+#[macro_use]
+extern crate prettytable;
 use hyper_tls::HttpsConnector;
+use std::{collections::HashMap, fmt::Display, ops::Add, path::PathBuf};
 
 use cargo_toml::Manifest;
-use glob::GlobError;
 use hyper::{
     body::{to_bytes, Buf},
     Client,
 };
-use tokio::io::{self, AsyncWriteExt as _};
 
-
-use prettytable::{Table, Row, Cell};
+use prettytable::Table;
 pub enum SourceType {
     Remote,
     Local,
@@ -28,144 +26,133 @@ pub struct SyncConfig {
 
 pub struct TomlSync {
     pub config: SyncConfig,
-    pub source_versions:HashMap<String,Vec<TargetInfo>>,
-    pub target_versions:HashMap<String,Vec<TargetInfo>>
+    pub source_versions: HashMap<String, Vec<TargetInfo>>,
+    pub target_versions: HashMap<String, Vec<TargetInfo>>,
 }
 #[derive(Debug)]
 pub struct TargetInfo {
-    pub path:String,
-    pub version:Option<String>,
+    pub path: String,
+    pub version: Option<String>,
 }
 
 impl Display for TargetInfo {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "path: {} \n versions: {:?}", self.path,self.version)
+        write!(f, "path: {} \n versions: {:?}", self.path, self.version)
     }
 }
 
-
-
-
-pub struct DependencyInfo{
-
-}
+pub struct DependencyInfo {}
 
 impl TomlSync {
-
     pub fn new(config: SyncConfig) -> Self {
-        TomlSync { 
-            config ,
-            source_versions:HashMap::new(),
-            target_versions:HashMap::new(),
+        TomlSync {
+            config,
+            source_versions: HashMap::new(),
+            target_versions: HashMap::new(),
         }
     }
 
-    pub async fn start_sync(&mut self){
-        let source_manifests=self.load_source_tomls().await;
-        let target_manifests =self.load_target_tomls().await;
+    pub async fn scan(&mut self) {
+        let source_manifests = self.load_source_tomls().await;
+        let target_manifests = self.load_tomls(self.config.destination.clone()).await;
         for manifest in source_manifests {
-            Self::extract_dependencies(&mut (*self).source_versions,manifest.0,manifest.1);
-            
+            Self::extract_dependencies(&mut (*self).source_versions, manifest.0, manifest.1);
         }
 
         for manifest in target_manifests {
-            Self::extract_dependencies(&mut (*self).target_versions ,manifest.0,manifest.1);
+            Self::extract_dependencies(&mut (*self).target_versions, manifest.0, manifest.1);
         }
     }
 
-    pub fn show_diff(&self){
+    pub fn show_diff(&self) {
         let mut table = Table::new();
-        table.add_row(row!["dependency", "target_versions","source_versions"]);
+        table.add_row(row!["dependency", "target_versions", "source_versions"]);
 
-        for entry in &self.target_versions{
-            let tversions=entry.1;
-            let target_versions_str=tversions.into_iter().map(Self::print_target_info).collect::<Vec<String>>().join("\n");
-            let source_version_str= self.source_versions.get(entry.0).map(|targets|{
-                targets.into_iter().map(Self::print_target_info).collect::<Vec<String>>().join("\n")
-            }).unwrap_or("".to_string());
-            table.add_row(row![entry.0,target_versions_str,source_version_str]);
+        for entry in &self.target_versions {
+            let tversions = entry.1;
+            let target_versions_str = tversions
+                .into_iter()
+                .map(Self::print_target_info)
+                .collect::<Vec<String>>()
+                .join("\n");
+            let source_version_str = self
+                .source_versions
+                .get(entry.0)
+                .map(|targets| {
+                    targets
+                        .into_iter()
+                        .map(Self::print_target_info)
+                        .collect::<Vec<String>>()
+                        .join("\n")
+                })
+                .unwrap_or("".to_string());
+            table.add_row(row![entry.0, target_versions_str, source_version_str]);
         }
         table.printstd();
-
-
-        
-        
-
     }
 
-    fn print_target_info(target_info: & TargetInfo)->String{
-        format!("path:{} \nversion: {}",target_info.path,target_info.version.clone().unwrap_or("none".to_string()))
+    fn print_target_info(target_info: &TargetInfo) -> String {
+        format!(
+            "path:{} \nversion: {}",
+            target_info.path,
+            target_info.version.clone().unwrap_or("none".to_string())
+        )
     }
 
-    
-
-    
-
-    pub fn extract_dependencies(map:&mut HashMap<String,Vec<TargetInfo>>,path:String,manifest:Manifest){
-
+    pub fn extract_dependencies(
+        map: &mut HashMap<String, Vec<TargetInfo>>,
+        path: String,
+        manifest: Manifest,
+    ) {
         for dependency in manifest.dependencies {
-            let dependency_key= dependency.0;
-            let version = dependency.1.detail().and_then(|d|d.version.clone());
-            println!("target dependency:{:?} version:{:?}",dependency_key,version);
-            let target_info =TargetInfo{
+            let dependency_key = dependency.0;
+            let version = dependency.1.detail().and_then(|d| d.version.clone());
+            println!("dependency:{:?} version:{:?}", dependency_key, version);
+            let target_info = TargetInfo {
                 version,
                 path: path.clone(),
-
             };
             map.entry(dependency_key)
-            .or_insert(vec![]).push(target_info);
-
-
+                .or_insert(vec![])
+                .push(target_info);
         }
-
     }
 
-    pub async fn load_source_tomls(&self) -> Vec<(String,Manifest)> {
-        let bytes = self.load_source_bytes().await;
-        return bytes
-            .into_iter()
-           
-            .filter_map(|(path,byte)| {
-                let result=Manifest::from_slice(&byte);
-                match result {
-                    Ok(manifest)=>Some((path,manifest)),
-                    Err(err)=>{
-                        println!("{:?}",err);
-                        None
-                    }
-                }
-            })
-            .collect::<Vec<(String,Manifest)>>();
-    }
-
-    pub async fn load_target_tomls(&self) -> Vec<(String,Manifest)> {
-        let pattern = format!("{}/**/Cargo.toml", &self.config.destination);
+    pub async fn load_tomls(&self, directory: String) -> Vec<(String, Manifest)> {
+        let pattern = format!("{}/**/Cargo.toml", directory);
         let globs = glob::glob(&pattern).expect("Invalid Glob expression");
         return globs
             .into_iter()
             .filter_map(|p| p.ok())
-            .filter_map(|pb| std::fs::read(pb.clone()).map(|bytes|{(bytes,pb)}).ok())
-            .filter_map(|(bytes,pb)| {
-                let result=Manifest::from_slice(&bytes);
+            .filter_map(|pb| std::fs::read(pb.clone()).map(|bytes| (bytes, pb)).ok())
+            .filter_map(|(bytes, pb)| {
+                let result = Manifest::from_slice(&bytes);
                 match result {
-                    Ok(manifest)=>Some((pb.clone().to_str().unwrap_or("").to_string(),manifest)),
-                    Err(err)=>{
-                        println!("{:?}",err);
+                    Ok(manifest) => Some((pb.clone().to_str().unwrap_or("").to_string(), manifest)),
+                    Err(err) => {
+                        println!("{:?}", err);
                         None
                     }
                 }
             })
-            .collect::<Vec<(String,Manifest)>>();
+            .collect::<Vec<(String, Manifest)>>();
     }
 
-    async fn load_source_bytes(&self) -> Vec<(String,Vec<u8>)> {
-        let mut source_bytes = Vec::<(String,Vec<u8>)>::new();
+    async fn load_source_tomls(&self) -> Vec<(String, Manifest)> {
+        let mut source_bytes = Vec::<(String, Manifest)>::new();
         for source in &self.config.sources {
             match source.source_type {
                 SourceType::Remote => {
                     if let Ok(uri) = source.path.parse() {
                         if let Ok(data) = Self::fetch_url(uri).await {
-                            source_bytes.push((source.path.clone(),data));
+                            // source_bytes.push((source.path.clone(),data));
+                            let result = Manifest::from_slice(&data);
+                            match result {
+                                Ok(manifest) => source_bytes.push((source.path.clone(), manifest)),
+                                Err(err) => {
+                                    println!("Failed to Parse {:?}", err);
+                                }
+                            }
                         } else {
                             println!("Failed to get data from {:?}", source.path)
                         }
@@ -174,11 +161,8 @@ impl TomlSync {
                     }
                 }
                 SourceType::Local => {
-                    if let Ok(res) = std::fs::read(source.path.clone()) {
-                        source_bytes.push((source.path.clone(),res));
-                    } else {
-                        println!("Failed To read from {:?}", source.path)
-                    }
+                    let mut res = self.load_tomls(source.path.clone()).await;
+                    source_bytes.append(&mut res);
                 }
             }
         }
@@ -187,8 +171,7 @@ impl TomlSync {
 
     async fn fetch_url(url: hyper::Uri) -> Result<Vec<u8>, hyper::Error> {
         let https = HttpsConnector::new();
-        let client = Client::builder()
-            .build::<_, hyper::Body>(https);
+        let client = Client::builder().build::<_, hyper::Body>(https);
 
         let mut res = client.get(url).await?;
 
@@ -202,11 +185,11 @@ impl TomlSync {
 
 #[cfg(test)]
 mod tests {
-    use crate::{SyncConfig, SourceType, Source, TomlSync};
+    use crate::{Source, SourceType, SyncConfig, TomlSync};
 
     #[tokio::test]
     async fn it_works() {
-       let config = SyncConfig{
+        let config = SyncConfig{
            sources:vec![Source{
                path:"https://raw.githubusercontent.com/paritytech/frontier/master/frame/evm/Cargo.toml".to_owned(),
                source_type:SourceType::Remote
@@ -214,10 +197,8 @@ mod tests {
            destination:"./../".to_owned(),
        };
 
-       let  mut toml_sync= TomlSync::new(config);
-       toml_sync.start_sync().await;
-       toml_sync.show_diff();
-
-
+        let mut toml_sync = TomlSync::new(config);
+        toml_sync.scan().await;
+        toml_sync.show_diff();
     }
 }
